a286351856488933f7b879a7883d72a0631ca687
nav <navneet.kumar@kvpcorp.com>
2016-03-04 13:16:50 +0530

    gem updation

--- db/schema.rb
+++ db/schema.rb
@@ -170,14 +170,14 @@ ActiveRecord::Schema.define(version: 20160304055408) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.text     "mobile"
-    t.integer  "posts_count"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "mobile"
+    t.integer  "posts_count"
     t.string   "slug"
   end
 
