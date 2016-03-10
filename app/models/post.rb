6c70f1914635e1f6b08bf52313c0961f95b5d4b5
nav <navneet.kumar@kvpcorp.com>
2016-03-10 18:01:16 +0530

    error fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,7 @@ class Post < ActiveRecord::Base
   mount_uploader :attachment, AvatarUploader
   validates_presence_of :content
   validates_presence_of :user
-  auto_html_for :content do
-    html_escape
-    image
-    youtube(width: 400, height: 250, autoplay: true)
-    link target: '_blank', rel: 'nofollow'
-    simple_format
- end
+  
 
 
 end
